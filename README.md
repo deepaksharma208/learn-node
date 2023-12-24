4b3c170df94ececdfda7b4dba1efe55d29d0028a
deepak <deepak@abicart.in>
2023-12-24 22:05:33 +0530

    Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-node
+
+Step 1: Make a DB app with MongoDB
---
+++ app.js
@@ -0,0 +1,7 @@
+//__dirname Global var
+console.log(__dirname);
+//__filename Global var
+console.log(__filename);
+
+global.myVar = 'Hello Node';
+console.log(myVar);
